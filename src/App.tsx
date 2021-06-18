c40a2e0b9945970cc77737a7af6a5722a76f2784
gurshant <gurshant53@gmail.com>
2021-06-19 01:24:31 +0530

    read msgs working with firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from './firebase';
+
+interface IState{
+  school:{
+    id: number;
+    title: string;
+  }[]
+}
+
 function App() {
+  const [schools, setSchools ] = useState<IState["school"]>([]);
+
+  const [loading, setLoading] = useState(true);
+
+  const ref = firebase.firestore().collection("school");
+
+  const getSchools=()=>{
+    setLoading(true);
+    ref.onSnapshot((querySnapshot)=>{
+      const items: any= [];
+      console.log(querySnapshot);
+      querySnapshot.forEach( (doc)=> {
+         items.push(doc.data());
+         console.log(doc.data());
+      })
+      setSchools(items);
+      // console.log(items);
+      setLoading(false);
+    })
+  }
+
+  useEffect(()=>{
+    getSchools();
+  }, []);
+  
+  if(loading){
+    return <h1>loading... </h1>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Graphs</h1>
+      {schools.map((school)=>(
+        <div key ={school.id}>
+          <h2>{school.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
